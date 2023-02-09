c9d839b0e5e306db85d3e79e7514a71ce20bc2e5
André Viapiana <andreviapiana@gmail.com>
2023-02-09 08:15:18 -0300

    💄Create Global Style

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  *::-webkit-scrollbar {
+        width: 22px;
+        height: 22px;
+        border-radius: 9999px;
+    }
+
+  *::-webkit-scrollbar-corner {
+      background-color: transparent;  
+  }
+
+  *::-webkit-scrollbar-thumb {
+      width: 6px;
+      background-color: transparent;
+      border-radius: 80px;
+      box-shadow: inset 0 0 0px 6px ${(props) => props.theme.blue};
+      border: solid 10px transparent;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.blue};
+  }
+
+  body {
+    background: ${(props) => props.theme["base-background"]};
+    color: ${(props) => props.theme["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1.6rem Nunito, sans-serif;
+  }
+`
