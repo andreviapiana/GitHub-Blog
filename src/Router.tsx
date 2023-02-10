664406bea0b3e11fde130df4ca26e2a318c28986
André Viapiana <andreviapiana@gmail.com>
2023-02-10 08:55:31 -0300

    ✨feat: Creation of Default Layout for Optimization

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { Home } from './pages/Home'
 import { Post } from './pages/Post'
@@ -6,8 +7,10 @@ import { Post } from './pages/Post'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post" element={<Post />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Post />} />
+      </Route>
     </Routes>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import { Header } from '../../components/Header'
 import { MainCard } from '../../components/MainCard'
 import { PostCard } from './components/PostCard'
 import { SearchInput } from './components/SearchInput'
@@ -7,7 +6,6 @@ import { HomeContainer, PostCardGrid } from './styles'
 export function Home() {
   return (
     <HomeContainer>
-      <Header />
       <MainCard />
       <SearchInput />
       <PostCardGrid>
