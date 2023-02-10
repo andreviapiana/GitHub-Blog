8cd3b94167ce045f5eae9dd9b80f82ffdd86de48
André Viapiana <andreviapiana@gmail.com>
2023-02-10 14:42:39 -0300

    ✨feat: Post page complete Creation

---
+++ src/pages/Post/PostContentCard/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const PostContentCardContainer = styled.section`
+  display: flex;
+`
+
+export const PostDetailsContainer = styled.div`
+  width: 86.4rem;
+  max-height: 45.6rem;
+  padding: 0 3.2rem;
+
+  margin-bottom: 5rem;
+
+  border-top: 4rem solid transparent;
+  border-bottom: 4rem solid transparent;
+
+  overflow: auto;
+  overflow: overlay;
+`
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -5,14 +5,3 @@ export const HomeContainer = styled.main`
   flex-direction: column;
   align-items: center;
 `
-
-export const PostDetailsContainer = styled.div`
-  width: 86.4rem;
-  max-height: 44rem;
-  padding: 4rem 3.2rem;
-
-  margin-bottom: 5rem;
-
-  overflow: auto;
-  overflow: overlay;
-`
