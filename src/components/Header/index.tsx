77088310c0ce3c3531ad831d1166cfa6f88009a0
André Viapiana <andreviapiana@gmail.com>
2023-02-10 10:17:10 -0300

    💄style: Add Link and Animation in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import { HeaderContainer, HeaderContent } from './styles'
 
 import logoImg from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt="" />
+        <NavLink to="/">
+          <img className="logo" src={logoImg} alt="" />
+        </NavLink>
       </HeaderContent>
     </HeaderContainer>
   )
