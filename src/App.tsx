1e75d2402b57bf306b676d3106fa491ba354739a
André Viapiana <andreviapiana@gmail.com>
2023-02-16 21:07:48 -0300

    💄style: Install and Apply React Loading Skeleton to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <BrowserRouter>
-        <Router />
+        <SkeletonTheme baseColor="#112131" highlightColor="#3294F8">
+          <Router />
+        </SkeletonTheme>
       </BrowserRouter>
     </ThemeProvider>
   )
